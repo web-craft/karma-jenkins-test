79e1f29492ea392352fd7e85fc98b72a7eb7577c
anton <smirnoff.anton@gmail.com>
2013-12-20 13:34:17 +0400

    fix test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -23,27 +23,7 @@ describe('Controller: MainCtrl', function () {
     expect(scope.awesomeThings.length).toBe(3);
   });
 
-
-  describe('Controller: MainCtrl21', function () {
-
-  // load the controller's module
-  beforeEach(module('angCiTestApp'));
-
-  var MainCtrl,
-    scope;
-
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
-  });
-});
+  
 });
 
 describe('Controller: MainCtrl2', function () {
